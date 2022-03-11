c4bfa15ed9a4df225af7673758895650f71c5ea5
Fernando Guerrero <devrrior@gmail.com>
2022-03-10 22:47:41 -0600

    feat(project): add RequireAuth route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyles } from './App.styles';
+import { ChatPage } from './pages/Chat/ChatPage/ChatPage';
 import { LoginPage } from './pages/Login/LoginPage/LoginPage';
+import { RequireAuth } from './utils/RequireAuth';
 
 function App() {
   return (
@@ -8,6 +10,14 @@ function App() {
       <GlobalStyles />
       <Routes>
         <Route path='login/' element={<LoginPage />} />
+        <Route
+          path='/'
+          element={
+            <RequireAuth>
+              <ChatPage />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
