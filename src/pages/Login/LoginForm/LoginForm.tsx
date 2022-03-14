0306c780a77eba5f9eb997456b9d6f6316072c1b
Fernando Guerrero <devrrior@gmail.com>
2022-03-14 13:23:36 -0600

    feat(loginform): use login funtcion onSubmit

--- src/pages/Login/LoginForm/LoginForm.tsx
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,16 +1,32 @@
-import { Button, Input } from "./LoginForm.style";
+import { useContext } from 'react';
+import { AuthContext } from '../../../context/AuthContext';
+import { LoginFormElement } from '../../../interfaces/formInterfaces';
+import { Button, Input } from './LoginForm.style';
 
 export const LoginForm = () => {
+  const { loginUser } = useContext(AuthContext);
+
+  const handleSubmit = (e: React.FormEvent<LoginFormElement>) => {
+    e.preventDefault();
+    const email = e.currentTarget.elements.email.value;
+    const password = e.currentTarget.elements.password.value;
+
+    loginUser(email, password);
+
+    e.currentTarget.elements.email.value = '';
+    e.currentTarget.elements.password.value = '';
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <Input type={'text'} placeholder='Email' />
+          <Input type={'text'} id='email' placeholder='Email' />
         </div>
         <div>
-          <Input type={'text'} placeholder='Password' />
+          <Input type={'password'} id='password' placeholder='Password' />
         </div>
-        <Button type="submit">Login</Button>
+        <Button type='submit'>Login</Button>
       </form>
     </div>
   );
