13554af46f41fda2d356f3859771cc48f4b874a4
Fernando Guerrero <devrrior@gmail.com>
2022-03-15 13:04:20 -0600

    feat(AuthContext.tsx): add logout function

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,12 @@
 import { createContext } from 'react';
+import { AuthState } from '../interfaces/authInterfaces';
 
 interface AuthContextProps {
+  authState: AuthState;
   loginUser: (email: string, password: string) => void;
+  logout: () => void;
 }
 
-export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+export const AuthContext = createContext<AuthContextProps>(
+  {} as AuthContextProps
+);
