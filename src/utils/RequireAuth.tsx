b3faa8273da648b97431ea6c9861ab46840aee55
Fernando Guerrero <devrrior@gmail.com>
2022-03-15 13:06:35 -0600

    fix(RequireAuth.tsx): using authState

--- src/utils/RequireAuth.tsx
+++ src/utils/RequireAuth.tsx
@@ -1,11 +1,12 @@
 import { useLocation, Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 export const RequireAuth = ({ children }: { children: JSX.Element }) => {
-  const auth = { user: null };
+  const { authState } = useAuth();
   const location = useLocation();
 
 
-  if (!auth.user) {
+  if (!authState.isAthenticathed) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
