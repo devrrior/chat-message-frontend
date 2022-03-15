86f6ffbc6d8773ca214a17ab5a3896035bbaa658
Fernando Guerrero <devrrior@gmail.com>
2022-03-15 13:05:45 -0600

    feat(LoginPage.tsx): add conditional if user is logged in, cant enter into login page

--- src/pages/Login/LoginPage/LoginPage.tsx
+++ src/pages/Login/LoginPage/LoginPage.tsx
@@ -8,8 +8,20 @@ import {
   Title,
 } from './LoginPage.styles';
 import { LoginForm } from '../LoginForm/LoginForm';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
+import { useEffect } from 'react';
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
+  const { authState } = useAuth();
+
+  useEffect(() => {
+    if (authState.isAthenticathed) {
+      navigate('/');
+    }
+  }, []);
+
   return (
     <Container>
       <ImgSide>
