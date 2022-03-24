37bef0c9300e0d86310e0b4e9edec63b3b17c917
Fernando Guerrero <devrrior@gmail.com>
2022-03-23 20:25:45 -0600

    style(loginform): remove interface

--- src/pages/Login/LoginForm/LoginForm.tsx
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../hooks/useAuth';
-import { LoginFormElement } from '../../../interfaces/formInterfaces';
 import { Button, Form, Input, Error } from './LoginForm.style';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
