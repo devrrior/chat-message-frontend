96dc1027a0c963e1a0c555ac983033e3c7bbc522
Fernando Guerrero <devrrior@gmail.com>
2022-03-26 00:25:21 -0600

    fix(ChatSide): pass props to components

--- src/pages/Chat/ChatSide/ChatSide.tsx
+++ src/pages/Chat/ChatSide/ChatSide.tsx
@@ -6,7 +6,10 @@ import { Messages } from './components/Messages/Messages';
 export const ChatSide = () => {
   return (
     <Container>
-      <InfoBar />
+      <InfoBar
+        contactName='Alexa Fernandez'
+        urlProfilePic='https://i.pinimg.com/564x/70/76/bc/7076bc3bb5422124f6e946287fc00e41.jpg'
+      />
       <Messages />
       <MessageBar />
     </Container>
