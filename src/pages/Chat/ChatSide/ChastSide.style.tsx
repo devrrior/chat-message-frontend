6ca482f99794f39f22af178596e2c48b6429c604
Fernando Guerrero <devrrior@gmail.com>
2022-03-25 23:32:11 -0600

    fix(ChatSide): fix grid template rows

--- src/pages/Chat/ChatSide/ChastSide.style.tsx
+++ src/pages/Chat/ChatSide/ChastSide.style.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: grid;
-  grid-template-rows: 8% auto 6%;
+  grid-template-rows: 8% 85% 7%;
 `;
