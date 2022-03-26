d62ad6b843655c314e9c2e47b1254c24ba99fd38
Fernando Guerrero <devrrior@gmail.com>
2022-03-26 00:24:43 -0600

    feat(InfoBar): pass props

--- src/pages/Chat/ChatSide/components/InfoBar/InfoBar.tsx
+++ src/pages/Chat/ChatSide/components/InfoBar/InfoBar.tsx
@@ -6,16 +6,20 @@ import {
 } from './InfoBar.style';
 import { HiDotsVertical } from 'react-icons/hi';
 
-export const InfoBar = () => {
+interface Props {
+  contactName: string;
+  urlProfilePic: string;
+}
+export function InfoBar({ contactName, urlProfilePic }: Props) {
   return (
     <Container>
       <ContainerPicAndName>
-        <ProfilePic src='https://i.pinimg.com/564x/70/76/bc/7076bc3bb5422124f6e946287fc00e41.jpg' />
-        <NameContact>Leslie Alexander</NameContact>
+        <ProfilePic src={urlProfilePic} />
+        <NameContact>{contactName}</NameContact>
       </ContainerPicAndName>
       <div>
         <HiDotsVertical fontSize='22px' cursor='pointer' color='#424242' />
       </div>
     </Container>
   );
-};
+}
