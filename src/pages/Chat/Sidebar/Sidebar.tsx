7b5dbd15ba37005304f644d2b0ea519f092c9832
Fernando Guerrero <devrrior@gmail.com>
2022-03-26 00:23:51 -0600

    fix(Sidebar): using with props the components

--- src/pages/Chat/Sidebar/Sidebar.tsx
+++ src/pages/Chat/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { ContactCard } from './components/ContactCard/ContactCard';
 import { SearchInput } from './components/SearchInput/SearchInput';
-import { Container } from './Sidebar.style';
+import { Container, ContainerContactCard } from './Sidebar.style';
 
 export const Sidebar = () => {
   return (
@@ -8,12 +8,22 @@ export const Sidebar = () => {
       <br />
       <SearchInput />
       <br />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
+      <ContainerContactCard>
+        <ContactCard
+          contactName='Linda Keebler'
+          urlProfilePic='https://static.intercomassets.com/avatars/435807/square_128/Me_profile_pic-1586948573.png?1586948573'
+          lastMessage='Laboris cillum pariatur culpa amet veniam proident id nisi incididunt pariatur dolore.'
+          lastMessageTime='13:32'
+          infoNotification={null}
+        />
+        <ContactCard
+          contactName='Linda Keebler'
+          urlProfilePic='https://static.intercomassets.com/avatars/435807/square_128/Me_profile_pic-1586948573.png?1586948573'
+          lastMessage='Laboris cillum pariatur culpa amet veniam proident id nisi incididunt pariatur dolore.'
+          lastMessageTime='13:32'
+          infoNotification={3}
+        />
+      </ContainerContactCard>
     </Container>
   );
 };
