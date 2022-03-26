d60c7744c0e9b180adc123e18030c02f7e2dfe66
Fernando Guerrero <devrrior@gmail.com>
2022-03-26 00:24:31 -0600

    feat(Messages.style): start scroll from the bottom

--- src/pages/Chat/ChatSide/components/Messages/Messages.style.tsx
+++ src/pages/Chat/ChatSide/components/Messages/Messages.style.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: column-reverse;
   padding: 10px 18px 10px 18px;
   overflow-y: scroll;
 `;
