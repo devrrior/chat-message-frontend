2c4c5d43f119e654076b38619d5b0bd65c5023db
Fernando Guerrero <devrrior@gmail.com>
2022-03-25 23:31:51 -0600

    feat(Message): create component

---
+++ src/pages/Chat/ChatSide/components/Messages/Messages.style.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 18px 10px 18px;
+  overflow-y: scroll;
+`;
---
+++ src/pages/Chat/ChatSide/components/Messages/Messages.tsx
@@ -0,0 +1,66 @@
+import { MyMessage } from './components/MyMessage';
+import { TheirMessage } from './components/TheirMessage';
+import { Container } from './Messages.style';
+
+export const Messages = () => {
+  return (
+    <Container>
+      <MyMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <TheirMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+
+      <MyMessage
+        message='Pariatur ullamco eiusmod cupidatat est ex ad. Laborum occaecat consequat qui veniam. Cillum do nulla et. Consectetur laborum cillum cupidatat velit. Cillum labore nostrud dolore. Proident est id et velit tempor consequat elit culpa esse ullamco eiusmod aliqua cillum ullamco. Quis laborum ipsum aute ipsum. Excepteur nostrud adipisicing dolore occaecat do est ex. Aliqua ullamco deserunt officia aliqua et fugiat enim minim do minim non ad do ipsum sint. Labore consequat ex proident officia ullamco enim pariatur eu tempor nulla qui ad.'
+        time='13:22'
+      />
+
+      <MyMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <TheirMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <TheirMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <TheirMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <TheirMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+
+      <MyMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <MyMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <MyMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+      <MyMessage
+        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        time='13:22'
+      />
+
+      <TheirMessage
+        message='Pariatur ullamco eiusmod cupidatat est ex ad. Laborum occaecat consequat qui veniam. Cillum do nulla et. Consectetur laborum cillum cupidatat velit. Cillum labore nostrud dolore. Proident est id et velit tempor consequat elit culpa esse ullamco eiusmod aliqua cillum ullamco. Quis laborum ipsum aute ipsum. Excepteur nostrud adipisicing dolore occaecat do est ex. Aliqua ullamco deserunt officia aliqua et fugiat enim minim do minim non ad do ipsum sint. Labore consequat ex proident officia ullamco enim pariatur eu tempor nulla qui ad.'
+        time='13:22'
+      />
+    </Container>
+  );
+};
