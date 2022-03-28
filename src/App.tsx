2471847532dc641437569f3121eed06140fa7fef
Fernando Guerrero <devrrior@gmail.com>
2022-03-27 23:39:48 -0600

    fix(context): modify files for AuthContext works fine

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import './index.css'
+import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyles } from './App.styles';
-import { AuthProvider } from './context/AuthProvider';
+import { AuthProvider } from './context/AuthContext/AuthProvider';
 import { ChatPage } from './pages/Chat/ChatPage/ChatPage';
 import { LoginPage } from './pages/Login/LoginPage/LoginPage';
 import { RequireAuth } from './utils/RequireAuth';
+import { WebsocketProvider } from './context/WebSocketContext/WebSocketProvider';
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <GlobalStyles />
-        <Routes>
-          <Route path='login/' element={<LoginPage />} />
-          <Route
-            path='/'
-            element={
-              <RequireAuth>
-                <ChatPage />
-              </RequireAuth>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <WebsocketProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <GlobalStyles />
+          <Routes>
+            <Route path='login/' element={<LoginPage />} />
+            <Route
+              path='/'
+              element={
+                <RequireAuth>
+                  <ChatPage />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </WebsocketProvider>
   );
 }
 
