fa74b39942a0cf6ac738e3db229102ee4c0c5bcc
Fernando Guerrero <devrrior@gmail.com>
2022-04-03 23:30:53 -0500

    feat(MessageBar): add functionality for render new messages

--- src/pages/Chat/ChatSide/components/MessageBar/MessageBar.tsx
+++ src/pages/Chat/ChatSide/components/MessageBar/MessageBar.tsx
@@ -5,12 +5,32 @@ import {
   SendText,
 } from './MessageBar.style';
 import { AiOutlineSend } from 'react-icons/ai';
+import { useWebSocket } from '../../../../../hooks/useWebSocket';
 
 export const MessageBar = () => {
+
+  const { websocket } = useWebSocket();
+
+  const handleSumit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formElements = form.elements as typeof form.elements & {
+      messageInput: HTMLInputElement;
+    };
+    const messageText = formElements.messageInput.value;
+
+    websocket.send(JSON.stringify({ message: messageText }));
+    formElements.messageInput.value = '';
+  };
   return (
-    <Container>
-      <MessageInput type='text' placeholder='Write a message' />
-      <SendButtonContainer>
+    <Container onSubmit={handleSumit}>
+      <MessageInput
+        id='messageInput'
+        type='text'
+        placeholder='Write a message'
+        autoComplete='off'
+      />
+      <SendButtonContainer type='submit'>
         <SendText>Send</SendText>
         <AiOutlineSend fontSize='22px' color='#f2f2f2' />
       </SendButtonContainer>
