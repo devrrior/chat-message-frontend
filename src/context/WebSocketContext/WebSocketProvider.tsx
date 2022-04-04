50182fa91055cb906896622cafc71020ab988d8b
Fernando Guerrero <devrrior@gmail.com>
2022-04-03 23:24:01 -0500

    fix(WebSocketProvider): fix when is creating websocket instance

--- src/context/WebSocketContext/WebSocketProvider.tsx
+++ src/context/WebSocketContext/WebSocketProvider.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
+import { useAuth } from '../../hooks/useAuth';
 import { WebSocketContext } from './WebSocketContext';
 
 interface WebsocketProviderProps {
   children: JSX.Element | JSX.Element[];
 }
 export const WebsocketProvider = ({ children }: WebsocketProviderProps) => {
-  // const roomName = 'test';
-  const [websocket, setWebsocket] = useState(
-    new WebSocket(`ws://127.0.0.1:8000/ws/chat/test/`)
+  const { authState } = useAuth();
+
+  const [accessToken, setAccessToken] = useState<string | null>(authState.accessToken);
+
+  const [url, setUrl] = useState<string>('ws://127.0.0.1:8000/ws/chat/test/');
+
+  const [websocket, setWebsocket] = useState<WebSocket>(
+    new WebSocket(url + '?token=' + accessToken)
   );
   return (
     <WebSocketContext.Provider value={{ websocket }}>
