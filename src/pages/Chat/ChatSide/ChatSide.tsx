7dee7af20e9f78e46f01aa7382cfe068b3a4c38f
Fernando Guerrero <devrrior@gmail.com>
2022-04-03 23:26:34 -0500

    feat(ChatSide): add function for refresh token when reload page

--- src/pages/Chat/ChatSide/ChatSide.tsx
+++ src/pages/Chat/ChatSide/ChatSide.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from 'react';
+import { axiosInstance } from '../../../api/axios';
+import { useAuth } from '../../../hooks/useAuth';
 import { Container } from './ChastSide.style';
 import { InfoBar } from './components/InfoBar/InfoBar';
 import { MessageBar } from './components/MessageBar/MessageBar';
 import { Messages } from './components/Messages/Messages';
 
+type responseStructure = { access: string; refresh: string };
+
 export const ChatSide = () => {
+  const { authState, newTokens } = useAuth();
+
+  useEffect(() => {
+    const refreshToken = async () => {
+      const response = await axiosInstance.post<responseStructure>(
+        'api/token/refresh/',
+        JSON.stringify({ refresh: authState.refreshToken }),
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+
+      newTokens(response.data.access, response.data.refresh);
+    };
+
+    refreshToken();
+    console.log('done');
+  }, []);
   return (
     <Container>
       <InfoBar
