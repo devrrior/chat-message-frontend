16bd7190ebf98d7d7148c8a0728bbb929e6025f0
Fernando Guerrero <devrrior@gmail.com>
2022-04-03 23:30:27 -0500

    feat(Messages): add functionality when send message

--- src/pages/Chat/ChatSide/components/Messages/Messages.tsx
+++ src/pages/Chat/ChatSide/components/Messages/Messages.tsx
@@ -1,66 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useWebSocket } from '../../../../../hooks/useWebSocket';
 import { MyMessage } from './components/MyMessage';
 import { TheirMessage } from './components/TheirMessage';
 import { Container } from './Messages.style';
 
 export const Messages = () => {
-  return (
-    <Container>
-      <MyMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <TheirMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
+  // const { websocket } = useWebSocket();
 
-      <MyMessage
-        message='Pariatur ullamco eiusmod cupidatat est ex ad. Laborum occaecat consequat qui veniam. Cillum do nulla et. Consectetur laborum cillum cupidatat velit. Cillum labore nostrud dolore. Proident est id et velit tempor consequat elit culpa esse ullamco eiusmod aliqua cillum ullamco. Quis laborum ipsum aute ipsum. Excepteur nostrud adipisicing dolore occaecat do est ex. Aliqua ullamco deserunt officia aliqua et fugiat enim minim do minim non ad do ipsum sint. Labore consequat ex proident officia ullamco enim pariatur eu tempor nulla qui ad.'
-        time='13:22'
-      />
 
-      <MyMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <TheirMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <TheirMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <TheirMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <TheirMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
+  const [messages, setMessages] = useState<Array<string>>([]);
 
-      <MyMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <MyMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <MyMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
-      <MyMessage
-        message='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        time='13:22'
-      />
+  const { websocket } = useWebSocket();
 
-      <TheirMessage
-        message='Pariatur ullamco eiusmod cupidatat est ex ad. Laborum occaecat consequat qui veniam. Cillum do nulla et. Consectetur laborum cillum cupidatat velit. Cillum labore nostrud dolore. Proident est id et velit tempor consequat elit culpa esse ullamco eiusmod aliqua cillum ullamco. Quis laborum ipsum aute ipsum. Excepteur nostrud adipisicing dolore occaecat do est ex. Aliqua ullamco deserunt officia aliqua et fugiat enim minim do minim non ad do ipsum sint. Labore consequat ex proident officia ullamco enim pariatur eu tempor nulla qui ad.'
-        time='13:22'
-      />
-    </Container>
-  );
+  useEffect(() => {
+    websocket.onopen = () => {
+      console.log('connected');
+    };
+
+    websocket.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      setMessages(() => [data.message, ...messages]);
+    };
+
+
+    websocket.onclose = function (e) {
+      console.error('Chat socket closed unexpectedly');
+    };
+  }, [messages]);
+
+  const renderMessages = (messages: Array<string>) => {
+    return messages.map((message, id) => {
+      return <MyMessage key={id} message={message} time='12:30' />;
+    });
+  };
+
+  return <Container>{renderMessages(messages)}</Container>;
 };
