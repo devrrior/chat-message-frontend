73d28cd885478c9ed78271a4e878a767463f4af8
Fernando Guerrero <devrrior@gmail.com>
2022-04-20 23:45:18 -0500

    fix(messages): fix scrollbar style

--- src/pages/Chat/ChatSide/components/Messages/Messages.style.tsx
+++ src/pages/Chat/ChatSide/components/Messages/Messages.style.tsx
@@ -5,9 +5,11 @@ export const Container = styled.div`
   flex-direction: column-reverse;
   padding: 10px 24px 10px 24px;
   overflow-y: scroll;
+  scrollbar-width: thin;
+  scrollbar-color: #c2c2c2 transparent;
 
   ::-webkit-scrollbar {
-    width: 5px;
+    width: 2px;
   }
   ::-webkit-scrollbar-thumb {
     background-color: #c2c2c2;
