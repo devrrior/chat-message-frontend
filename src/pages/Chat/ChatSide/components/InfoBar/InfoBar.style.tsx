637dd03a64cb58a43fa45a98fbb8100c57511547
Fernando Guerrero <devrrior@gmail.com>
2022-04-26 16:57:41 -0500

    fix(InfoBar.style): fix opacity of border

--- src/pages/Chat/ChatSide/components/InfoBar/InfoBar.style.tsx
+++ src/pages/Chat/ChatSide/components/InfoBar/InfoBar.style.tsx
@@ -7,7 +7,7 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   padding: 12px 24px;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
 `;
 
 export const ContainerPicAndName = styled.div`
